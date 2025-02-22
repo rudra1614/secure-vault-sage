885533bb748c2b7659be3ea4926888bfdaed1c95
rudra1614 <68456434+rudra1614@users.noreply.github.com>
2025-02-22 15:10:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
                 session === null ? null : session ? (
                   <Index />
                 ) : (
-                  <Navigate to="" replace />
+                  <Navigate to="/root" replace />
                 )
               }
             />
